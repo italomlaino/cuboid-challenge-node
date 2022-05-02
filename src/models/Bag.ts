78755f598daab86787217b28c0f09330f8ee01bc
italomlaino <italomlaino@gmail.com>
2022-05-02 08:52:03 -0300

    calculate bag available / payload volume before updating / inserting

--- src/models/Bag.ts
+++ src/models/Bag.ts
@@ -1,6 +1,7 @@
-import { Id, RelationMappings } from 'objection';
+import { Id, ModelOptions, QueryContext, RelationMappings } from 'objection';
 import { Cuboid } from './Cuboid';
 import Base from './Base';
+import _ from 'lodash';
 
 export class Bag extends Base {
   id!: Id;
@@ -9,6 +10,31 @@ export class Bag extends Base {
   payloadVolume!: number;
   availableVolume!: number;
   cuboids?: Cuboid[] | undefined;
+
+  async $beforeUpdate(
+    opt: ModelOptions,
+    queryContext: QueryContext
+  ): Promise<void> {
+    await super.$beforeUpdate(opt, queryContext);
+    this.updateVolume();
+  }
+
+  async $beforeInsert(queryContext: QueryContext): Promise<void> {
+    await super.$beforeInsert(queryContext);
+    this.updateVolume();
+  }
+
+  updateVolume(): void {
+    if (_.isNil(this.cuboids)) {
+      return;
+    }
+
+    this.payloadVolume = this.cuboids.reduce(
+      (volume: number, cuboid: Cuboid) => volume + cuboid.updateVolume(),
+      0
+    );
+    this.availableVolume = this.volume - this.payloadVolume;
+  }
 
   static tableName = 'bags';
 
